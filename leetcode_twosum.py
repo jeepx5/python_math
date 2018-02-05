a7098510e2843ecf0e564c518a849cb90d58244d
jeepx5 <jimway2002@hotmail.com>
2018-02-05 13:18:04 +0800

    Update leetcode_twosum.py

--- leetcode_twosum.py
+++ leetcode_twosum.py
@@ -1,17 +1,11 @@
 class Solution:
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         left=nums.copy()
         for i in nums:
             anth=target-i
             left.pop(0)
             if anth in left:
-                return nums.index(i), nums.index(anth)
+                print(left.index(anth))
+                return nums.index(i), (nums.index(i)+1+left.index(anth))
             else:
                 continue
-            
-            
