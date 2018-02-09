14246bc1e1ef7f58ed0575639563bc01cddef19b
jeepx5 <jimway2002@hotmail.com>
2018-02-09 16:28:12 +0800

    Update rome2int.py

--- rome2int.py
+++ rome2int.py
@@ -8,54 +8,49 @@ def rometoint(s):
            C, CC, CCC, CD, D, DC, DCC, DCCC, CM, M.
            """
 
-    rn={}
-    rn['M']=1000
+
+    rn = {}
+    rn['M'] = 1000
     rn['D'] = 500
     rn['C'] = 100
-    rn['L']=50
-    rn['X']=10
-    rn['V']=5
-    rn['I']=1
-    tmp=s[0]
-    rst=0
-    i=1
-    while i <= len(s)-1:
-        tmp = rn[s[i-1]]
-        print(i, tmp, rst)
-        if tmp==1000:
-            rst+=tmp
+    rn['L'] = 50
+    rn['X'] = 10
+    rn['V'] = 5
+    rn['I'] = 1
+    tmp = s[0]
+    rst = 0
+    i = 0
+    while i <= len(s) - 2:
+        # print('====', rst, i, tmp)
+        if rn[s[i]] >= rn[s[i+1]]:
+            rst += rn[s[i]]
             i+=1
-            continue
-        if rn[s[i]] > tmp:
-            rst = rst+rn[s[i]]-tmp
-            i+=2
-            continue
-        elif rn[s[i]] == tmp :
-            rst =rst+tmp+tmp
-            if i == len(s)-2:
-                i+=1
-                rst -= tmp
-            else:
-                i+=2
-
-            continue
-        elif rn[s[i]] < tmp:
-            rst = rst + rn[s[i]] + tmp
-            i+=2
-        #print(i, rst)
-        if i == len(s):
-            rst +=rn[s[-1]]
-
+        else:
+            rst -= rn[s[i]]
+            i+=1
+    rst+=rn[s[i]]
     return rst
+
+
+def romanToInt(s):
+    roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
+    z = 0
+    for i in range(0, len(s) - 1):
+        if roman[s[i]] < roman[s[i+1]]:
+            z -= roman[s[i]]
+        else:
+            z += roman[s[i]]
+    return z + roman[s[-1]]
 
 
 def main():
     print('start main')
-    print(rometoint("DCXXI"))
-    print(rometoint("MCMXCVI"))
-    print(rometoint("MDCCCLXXXIV"))
-    print(rometoint("MDLXX"))
-    print(rometoint("MCDLXXVI"))
+    print(rometoint("DCXXI"), romanToInt("DCXXI"))
+    print(rometoint("MCMXCVI"), romanToInt("MCMXCVI"))
+    print(rometoint("MDCCCLXXXIV"), romanToInt("MDCCCLXXXIV"))
+    print(rometoint("MDLXX"), romanToInt("MDLXX"))
+    print(rometoint("MCDLXXVI"), romanToInt("MCDLXXVI"))
+    print(rometoint("MMCCCXCIX"), romanToInt("MMCCCXCIX"))
     print('end main')
 
 if __name__ == '__main__':
